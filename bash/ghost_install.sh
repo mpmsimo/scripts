5e0e25dcf8dce776089ecbb675f34cc6c0b0b494
Michael Simo <os@mpmsimo.com>
2016-09-09 05:49:53 +0000

    Updated ghost install to change blog url

---
+++ bash/ghost_install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#mpmsimo
+#9/9/2016
+
+PACKAGE_ARRAY=(wget aasdf)
+GHOST_ZIP='https://ghost.org/zip/ghost-latest.zip'
+WEB_DIR='/var/www'
+GHOST_DIR='/var/www/ghost'
+DOMAIN='http://blog.mpmsimo.com'
+
+check_for_missing_packages(){
+    # figure out if needed packages are installed
+
+	for package in ${PACKAGE_ARRAY[@]};
+	do
+        #PKG_SEARCH=$(dpkg -s $package > /dev/null 2>&1)
+        PKG_SEARCH=$(dpkg-query -W -f='${Status}' $package 2> /dev/null | grep -c 'ok installed')
+        if [[ $PKG_SEARCH -eq 0 ]];
+            then
+                echo "Installing $package"
+                sudo apt-get install $package
+            else
+                echo "$package is already installed."
+        fi
+	done
+}
+
+ghost_install(){
+    sudo mkdir -p $GHOST_DIR
+    cd $WEB_DIR
+    #sudo wget $GHOST_ZIP
+    #sudo unzip -d ghost ghost-latest.zip
+    cd $GHOST_DIR
+
+    # Install node production dependencies
+    #sudo npm install --production
+
+    # Install example config
+    sudo cp config.example.js config.js
+
+    # Use sed to change url, use , instead / as delimeter because urls
+    echo "Changing blog URL to: $DOMAIN"
+    sudo sed -i -e "s,url\:.*,url\: '$DOMAIN',g" config.js
+}
+
+check_for_missing_packages
+ghost_install
